7dd58bf7da293a79cab4ec4c9a8168b5334e7225
p18239 <sjc18239@gmail.com>
2019-07-19 14:05:55 +0800

    Create 5.js

---
+++ js/5.js
@@ -0,0 +1,3 @@
+document.getElementById("demo3").innerHTML = Math.PI;
+document.getElementById("demo4").innerHTML = Math.round(6.85323)+Math.round(2.3555555) + Math.pow(8, 2) +Math.sqrt(64) +Math.abs(-4.7)+Math.min(0, 450, 35, 10, -8, -300, -78)+Math.max(0, 450, 35, 10, -8, -300, -78); 
+document.getElementById("demo5").innerHTML = Math.floor(Math.random() * 9999) + 1;
