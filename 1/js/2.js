941e4c97717e0a17c525f82dcd15b0b2144bd213
p18239 <sjc18239@gmail.com>
2019-07-25 10:45:32 +0800

    Create 2.js

---
+++ 1/js/2.js
@@ -0,0 +1,10 @@
+var person = {
+    s1 : "hi",
+    s2  : "hello",
+    s3       : 123,
+    s4  : "hey"
+};
+
+document.getElementById("demo").innerHTML =
+person.s1 + "<br>" + person.s2 + "<br>" +
+person.s3  + "<br>"+ person.s4;
